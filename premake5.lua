2ba91f7ae39e7bc99db9abee46a8d2d5288dd8f9
kateshiAIV <148715019+kateshiAIV@users.noreply.github.com>
2026-06-27 19:23:53 +0200

    premake

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "GameEngine"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "GameEngine"
+    location "GameEngine"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files 
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+    buildoptions
+    {
+        "/utf-8"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++23"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines 
+        {
+            "ENG_PLATFORM_WINDOWS",
+            "ENG_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines "ENG_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "ENG_RELEASE"
+        symbols "On"
+
+    filter "configurations:Dist"
+        defines "ENG_DIST"
+        symbols "On"
+
+
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    files 
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+    buildoptions
+    {
+        "/utf-8"
+    }
+    includedirs
+    {
+        "GameEngine/vendor/spdlog/include",
+        "GameEngine/src"
+    }
+    links
+    {
+        "GameEngine"
+    }
+    filter "system:windows"
+        cppdialect "C++23"
+        staticruntime "On"
+        systemversion "latest"
+        defines 
+        {
+            "ENG_PLATFORM_WINDOWS"
+        }
+    filter "configurations:Debug"
+        defines "ENG_DEBUG"
+        symbols "On"
+    filter "configurations:Release"
+        defines "ENG_RELEASE"
+        symbols "On"
+    filter "configurations:Dist"
+        defines "ENG_DIST"
+        symbols "On"
